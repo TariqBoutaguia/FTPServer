afe18f0d6663e95bd5ff7f0059e2c75381c3d99b
TariqBoutaguia <156303107+TariqBoutaguia@users.noreply.github.com>
2024-02-01 11:13:17 +0100

    FTPSetup.java

--- FTPSetup.java
+++ FTPSetup.java
@@ -1,8 +1,10 @@
 package serverFTP;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
 import java.util.Scanner;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -19,22 +21,25 @@ public class FTPSetup {
 	    try (ServerSocket serverSocket = new ServerSocket(port)) {
 	        while (true) {
 	            try (Socket clientSocket = serverSocket.accept()) {
-	                Out("220 Server ready.\r\n", clientSocket);
+	                Out("220 Service ready.\r\n", clientSocket);
 	                String login = In(clientSocket);
 	                
-	                Out("331 enter password : \r\n", clientSocket);
+	                Out("331 User name okay, need password\r\n", clientSocket);
 	                String passwd = In(clientSocket);
 	                
                     String loginParDefaut = "USER a";
                     String passParDefaut = "PASS 1";
                     
 	                if (login.equals(loginParDefaut) && passwd.equals(passParDefaut)) {
-	                Out("230 user logged in, proceed.\r\n", clientSocket);
+	                        Out("230 user logged in, proceed.\r\n", clientSocket);
+	                        run(clientSocket);
 	                        //handleClient(clientSocket);
-	                    } else {
+	                      
+	                } else {
 	                        Out("530 Not logged in.\r\n", clientSocket);
-	                    }
+	                        
 	                }
+	             }
 	             catch (IOException e) {
 	                e.printStackTrace();
 	            }
@@ -42,14 +47,62 @@ public class FTPSetup {
 	    } catch (IOException e) {
 	        e.printStackTrace();
 	    }
- }
+	}
+ 
+ 
+ public void run(Socket socket) {
+		try {
+			String command;
+			while ((command = In(socket)) != null) {
+				switch (command.toUpperCase()) {
+				case "QUIT":
+					Out("221 closing connection\r\n", socket);
+					socket.close();
+					break;
+				case "GET":
+					// Retrieve the filename from the client
+					String filename = In(socket);
+					File file = new File(filename);
+					if (file.exists()) {
+						// Read the file and send it to the client
+						byte[] fileBytes = Files.readAllBytes(file.toPath());
+						OutputStream outputStream = socket.getOutputStream();
+						outputStream.write(fileBytes);
+						Out("150 File sent successfully\r\n", socket);
+					} else {
+						Out("550 Error: File not found\r\n", socket);
+					}
+                    break;
+				case "CD":
+              // Change the current directory to the specified directory
+         		String currentDirectory = System.getProperty("user.dir");
+         		String directory = currentDirectory + "\\" + In(socket);
+         		Out(directory, socket);
+         		File folder = new File(directory);
+         		if (folder.exists() && folder.isDirectory()) {
+         			Out(System.setProperty("user.dir", directory), socket);
+         			System.getProperty("user.dir");
+         			Out("250 Directory changed to: " + directory, socket);
+         			// sendCommand("Directory changed to: " + directory, socket);
+         		} else {
+         			Out("550 Invalid directory", socket);
+         		}
+				default:
+					Out("500 Invalid command\r\n", socket);
+					break;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
  
 	private String In(Socket socket) throws IOException {
 		   InputStream in = socket.getInputStream();
 	       Scanner scan = new Scanner(in);
 	       String str = scan.nextLine();
-	       scan.close();
+	       //scan.close();
 	       return str;
 	}
 
@@ -58,6 +111,8 @@ public class FTPSetup {
 		out.write(str.getBytes());
 		out.flush();
 	}
+	
+	
  
  /*private boolean authentification(Socket socket) throws IOException {
 	 
@@ -76,10 +131,10 @@ public class FTPSetup {
 
 	
  
- private void handleClient(Socket socket) throws IOException {
+ /*private void handleClient(Socket socket) throws IOException {
      try{
          String text;
-         //code de test
+
          do {
              text = In(socket);
              String reverseText = new StringBuilder(text).reverse().toString();
@@ -89,5 +144,5 @@ public class FTPSetup {
      } finally {
          socket.close();
      }
- }
+ }*/
 }
